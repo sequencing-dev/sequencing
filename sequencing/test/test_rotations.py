8bb1055c8e099dc6e6681fa3705616fc9bf95fdc
qci-chou <chou@quantumcircuits.com>
2021-07-07 00:04:43 -0400

    KSC: Adds more detailed tests

--- sequencing/test/test_rotations.py
+++ sequencing/test/test_rotations.py
@@ -23,6 +23,7 @@ from sequencing.sequencing import (
     SyncOperation,
     HamiltonianChannels,
 )
+from sequencing.calibration import tune_rabi
 
 
 class TestSequenceVsUnitary(unittest.TestCase):
@@ -30,6 +31,7 @@ class TestSequenceVsUnitary(unittest.TestCase):
     def setUpClass(cls):
         qubit = Transmon("qubit", levels=2)
         system = System("system", modes=[qubit])
+        _ = tune_rabi(system, system.ground_state(), plot=False, verify=False)
         cls.system = system
 
     def test_sequence_Rx(self):
@@ -39,14 +41,19 @@ class TestSequenceVsUnitary(unittest.TestCase):
 
         init_state = system.ground_state()
 
-        seq = Sequence(system)
-        qubit.rotate_x(np.pi / 2)
-        sync()
+        angles = np.linspace(-np.pi, np.pi, 11)
+        for theta in angles:
+            seq = Sequence(system)
+            qubit.rotate_x(theta)
+            sync()
+            unitary = qubit.rotate_x(theta, unitary=True)
 
-        result = seq.run(init_state)
-        states = result.states
-        fidelity = qutip.fidelity(states[-1], qubit.Rx(np.pi / 2) * init_state) ** 2
-        self.assertGreater(fidelity, 0.999)
+            result = seq.run(init_state)
+            states = result.states
+            fidelity = qutip.fidelity(states[-1], qubit.Rx(theta) * init_state) ** 2
+            self.assertGreater(fidelity, 0.999)
+            fidelity = qutip.fidelity(states[-1], unitary * init_state) ** 2
+            self.assertGreater(fidelity, 0.999)
 
     def test_sequence_Ry(self):
 
@@ -55,14 +62,19 @@ class TestSequenceVsUnitary(unittest.TestCase):
 
         init_state = system.ground_state()
 
-        seq = Sequence(system)
-        qubit.rotate_y(np.pi / 2)
-        sync()
+        angles = np.linspace(-np.pi, np.pi, 11)
+        for theta in angles:
+            seq = Sequence(system)
+            qubit.rotate_y(theta)
+            sync()
+            unitary = qubit.rotate_y(theta, unitary=True)
 
-        result = seq.run(init_state)
-        states = result.states
-        fidelity = qutip.fidelity(states[-1], qubit.Ry(np.pi / 2) * init_state) ** 2
-        self.assertGreater(fidelity, 0.999)
+            result = seq.run(init_state)
+            states = result.states
+            fidelity = qutip.fidelity(states[-1], qubit.Ry(theta) * init_state) ** 2
+            self.assertGreater(fidelity, 0.999)
+            fidelity = qutip.fidelity(states[-1], unitary * init_state) ** 2
+            self.assertGreater(fidelity, 0.999)
 
     def test_sequence_Raxis(self):
 
@@ -70,17 +82,65 @@ class TestSequenceVsUnitary(unittest.TestCase):
         qubit = system.qubit
 
         init_state = system.ground_state()
-        theta = np.pi * 0.456
-        phi = -np.pi * 0.567
 
-        seq = Sequence(system)
-        qubit.rotate(theta, phi)
-        sync()
+        angles = np.linspace(-np.pi, np.pi, 11)
+        for theta in angles:
+            for phi in angles:
+                seq = Sequence(system)
+                qubit.rotate(theta, phi)
+                sync()
+                unitary = qubit.rotate(theta, phi, unitary=True)
 
-        result = seq.run(init_state)
-        states = result.states
-        fidelity = qutip.fidelity(states[-1], qubit.Raxis(theta, phi) * init_state) ** 2
-        self.assertGreater(fidelity, 0.999)
+                result = seq.run(init_state)
+                states = result.states
+                fidelity = (
+                    qutip.fidelity(states[-1], qubit.Raxis(theta, phi) * init_state)
+                    ** 2
+                )
+                self.assertGreater(fidelity, 0.999)
+                fidelity = qutip.fidelity(states[-1], unitary * init_state) ** 2
+                self.assertGreater(fidelity, 0.999)
+
+    def test_sequence_Raxis_vs_Rx_Ry(self):
+
+        system = self.system
+        qubit = system.qubit
+
+        init_state = system.ground_state()
+
+        angles = np.linspace(-np.pi, np.pi, 11)
+        for theta in angles:
+            # Test Raxis vs. Rx
+            seq = Sequence(system)
+            qubit.rotate(theta, 0)
+            sync()
+            unitary = qubit.rotate(theta, 0, unitary=True)
+
+            result = seq.run(init_state)
+            states = result.states
+            fidelity = qutip.fidelity(states[-1], qubit.Rx(theta) * init_state) ** 2
+            self.assertGreater(fidelity, 0.999)
+
+            fidelity = (
+                qutip.fidelity(unitary * init_state, qubit.Rx(theta) * init_state) ** 2
+            )
+            self.assertGreater(fidelity, 0.999)
+
+            # Test Raxis vs. Ry
+            seq = Sequence(system)
+            qubit.rotate(theta, np.pi / 2)
+            sync()
+            unitary = qubit.rotate(theta, np.pi / 2, unitary=True)
+
+            result = seq.run(init_state)
+            states = result.states
+            fidelity = qutip.fidelity(states[-1], qubit.Ry(theta) * init_state) ** 2
+            self.assertGreater(fidelity, 0.999)
+
+            fidelity = (
+                qutip.fidelity(unitary * init_state, qubit.Ry(theta) * init_state) ** 2
+            )
+            self.assertGreater(fidelity, 0.999)
 
 
 if __name__ == "__main__":
